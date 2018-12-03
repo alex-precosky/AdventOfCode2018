b6e6f1a45412506e32bb7ecee407acfe1d81b4c6
Alex Precosky <precosky@mac.com>
2018-12-02 22:12:35 -0800

    Day 3

---
+++ Day3.py
@@ -0,0 +1,88 @@
+import re
+import numpy as np
+
+class rectangle:
+    def __init__(self, claim_id, left, top, width, height):
+        self.claim_id = claim_id
+        self.left = left
+        self.top = top
+        self.width = width
+        self.height = height
+
+    def __eq__(self, other):
+        if (self.claim_id == other.claim_id) and (self.left == other.left) and (self.top == other.top) and (self.width == other.width) and (self.height
+                                                                     == other.height):
+            return True
+        else:
+            return False
+
+    @classmethod
+    def from_str(self, rectangle_str):
+        tok = re.split(' |,|x|:', rectangle_str)
+        
+        claim_id = int(re.search('(?<=#)\d+', tok[0]).group(0))
+
+        left = int(tok[2])
+        top = int(tok[3])
+        width = int(tok[5])
+        height = int(tok[6])
+
+        new_rect = rectangle(claim_id, left, top, width, height)
+        return new_rect
+        
+    def __repr__(self):
+        return f'ID: {self.claim_id} L: {self.left} T: {self.top} W: {self.width} H: {self.height}'
+
+def place_rectangle_on_fabric(the_rectangle, fabric):
+    for i in range(the_rectangle.left, the_rectangle.left + the_rectangle.width):
+        for j in range(the_rectangle.top, the_rectangle.top + the_rectangle.height):
+            fabric[j, i] += 1
+
+    return fabric
+
+def count_overlap(fabric):
+    overlap = 0
+
+    for x in np.nditer(fabric):
+        if x > 1:
+            overlap += 1
+
+    return overlap
+
+
+def does_rect_overlap(the_rectangle, fabric):
+    overlap = False
+
+    for i in range(the_rectangle.left, the_rectangle.left + the_rectangle.width):
+        for j in range(the_rectangle.top, the_rectangle.top + the_rectangle.height):
+            if fabric[j, i] > 1:
+                overlap = True
+
+    return overlap
+
+def main():
+    FABRIC_SIZE = 1000
+
+    fabric = np.zeros([FABRIC_SIZE, FABRIC_SIZE])
+
+    rectangles = []
+    for line in open("input/Day3.txt").readlines():
+        rectangles.append(rectangle.from_str(line.strip()))
+
+    for rect in rectangles:
+        fabric = place_rectangle_on_fabric(rect, fabric)
+
+    overlap_area = count_overlap(fabric)
+
+    print(f'Overlap: {overlap_area}')
+
+    claim_id_non_overlap = -1
+    for rect in rectangles:
+        if does_rect_overlap(rect, fabric) is False:
+            claim_id_non_overlap = rect.claim_id
+            break
+
+    print(f'Claim ID with no overlap: {claim_id_non_overlap}')
+
+if __name__ == "__main__":
+    main()
---
+++ test/test_day3.py
@@ -0,0 +1,54 @@
+import numpy as np
+import unittest
+from Day3 import rectangle, place_rectangle_on_fabric, count_overlap
+
+class Test(unittest.TestCase):
+
+    def test_parse_square(self):
+        target = '#12 @ 604,670: 22x16'
+
+        expected = rectangle(12, 604, 670, 22, 16)
+        actual = rectangle.from_str(target)
+
+        print(expected)
+        print(actual)
+
+        assert expected == actual
+
+    def test_place_rectangle_on_fabric(self):
+
+        target = rectangle(1, 2, 1, 2, 2)
+        fabric = np.zeros([4, 4])
+ 
+        expected = np.array([[0, 0, 0, 0],
+                             [0, 0, 1, 1],
+                             [0, 0, 1, 1],
+                             [0, 0, 0, 0]])
+        actual = place_rectangle_on_fabric(target, fabric)
+
+        assert (expected == actual).all()
+
+    def test_place_rectangle_on_fabric_twice(self):
+
+        target = rectangle(1, 1, 1, 2, 2)
+        fabric = np.zeros([4, 4])
+ 
+        expected = np.array([[0, 0, 0, 0],
+                             [0, 2, 2, 0],
+                             [0, 2, 2, 0],
+                             [0, 0, 0, 0]])
+        fabric = place_rectangle_on_fabric(target, fabric)
+        actual = place_rectangle_on_fabric(target, fabric)
+
+        assert (expected == actual).all()
+
+    def test_count_overlap(self):
+
+        target = np.array([[0, 0, 0, 0],
+                           [0, 2, 2, 0],
+                           [0, 1, 1, 0],
+                           [0, 0, 0, 0]])
+        expected = 2
+        actual = count_overlap(target)
+
+        assert expected == actual
