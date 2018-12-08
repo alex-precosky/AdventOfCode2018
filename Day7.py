139dcda269070765271decd67ff5756b5c9102b1
Alex Precosky <precosky@mac.com>
2018-12-08 12:27:49 -0800

    Completed day 7

--- Day7.py
+++ Day7.py
@@ -1,5 +1,6 @@
 import copy
 from heapq import heappush, heappop
+import ipdb
 import queue
 import re
 
@@ -20,10 +21,14 @@ class Worker:
         self.time_left = 0
         self.work_item = Node('-')
 
-def get_next_work_item(heap, remaining, finished):
+def get_next_work_item(heap, remaining, in_progress, finished):
+
     # pop items til we get one that is available
     is_available = False
-    while not is_available:
+    put_back = []
+    node = None
+
+    while not is_available and len(heap) > 0:
         put_back = []
         node = heappop(heap)
 
@@ -31,21 +36,23 @@ def get_next_work_item(heap, remaining, finished):
         for letter, letter_node in remaining.items():
             if node in letter_node.children and letter_node is not node:
                 is_available = False
+
+        for letter, letter_node in in_progress.items():
+            if node in letter_node.children and letter_node is not node:
+                is_available = False
             
         if not is_available:
             put_back.append(node)
+            node = None
         else:
             del remaining[node.letter]
-            finished.append(node)
-            for item in put_back:
-                if item not in heap:
-                    heappush(heap, item)
-
-    for child in node.children:
-        if child not in finished and child not in heap:
-            heappush(heap, child)
+            in_progress[node.letter] = node
         
-    return node, heap, remaining, finished
+    for item in put_back:
+        if item not in heap:
+            heappush(heap, item)
+        
+    return node, heap, remaining, in_progress, finished
 
 
 def priority_graph_traversal(graph_heads, graph_dict, num_workers=1):
@@ -56,6 +63,7 @@ def priority_graph_traversal(graph_heads, graph_dict, num_workers=1):
         heappush(heap, head)
 
     remaining = graph_dict
+    in_progress = {}
     finished = []
 
     available_worker_queue = queue.Queue()
@@ -70,12 +78,17 @@ def priority_graph_traversal(graph_heads, graph_dict, num_workers=1):
 
         total_time += 1
 
-        if(not available_worker_queue.empty()):
-            worker = available_worker_queue.get()
-            work_item, heap, remaining, finished = get_next_work_item(heap, remaining, finished)
-            worker.time_left = ord(work_item.letter)-4
-            worker.work_item = work_item
-            working_workers.append(worker)
+        # assign work to workers as long as there is work and workers available
+        while (not available_worker_queue.empty()):
+            work_item, heap, remaining, in_progress, finished = get_next_work_item(heap, remaining, in_progress, finished)
+
+            if work_item is not None:
+                worker = available_worker_queue.get()
+                worker.time_left = ord(work_item.letter)-4
+                worker.work_item = work_item
+                working_workers.append(worker)
+            else:
+                break
 
 
         for worker in working_workers:
@@ -83,10 +96,16 @@ def priority_graph_traversal(graph_heads, graph_dict, num_workers=1):
             if worker.time_left == 0:
                 steps.append(worker.work_item.letter)
                 available_worker_queue.put(worker)
-                working_workers.remove(worker)
 
-    print(total_time)
-    return ''.join(steps)
+                del in_progress[worker.work_item.letter]
+
+                for child in worker.work_item.children:
+                    if child not in finished and child not in heap:
+                        heappush(heap, child)
+
+        working_workers = [worker for worker in working_workers if worker.time_left != 0]
+
+    return ''.join(steps), total_time
 
 def get_letters_from_line(line):
     match = re.search(r'(?<=Step )([A-Z])', line)
@@ -116,18 +135,19 @@ def main():
             graph_dict[second_letter] = Node(second_letter)
 
         graph_dict[first_letter].children.append(graph_dict[second_letter])
-    
+
     
     first_letters.difference_update(second_letters)
     graph_heads = []
     for letter in first_letters:
         graph_heads.append(graph_dict[letter])
 
-    part1_order = priority_graph_traversal(graph_heads, graph_dict, num_workers=1)
-    print(part1_order)
+    part1_order, part1_time = priority_graph_traversal(graph_heads, dict(graph_dict), num_workers=1)
+    print(f'Order: {part1_order} Time: {part1_time} s')
 
-    part2_order = priority_graph_traversal(graph_heads, graph_dict, num_workers=5)
-    print(part2_order)
+    part2_order, part2_time = priority_graph_traversal(graph_heads, dict(graph_dict), num_workers=5)
+    print(f'Order: {part2_order} Time: {part2_time} s')
 
 if __name__ == "__main__":
     main()
+
